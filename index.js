33f487be1440c9708865e377c3a857c59479088f
valentinohrynt <hariyantovalentino@gmail.com>
2024-11-04 21:29:56 +0700

    add more discord text-channel id placeholder, so you can adjust it for more server you have

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js")
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
-const config = require("./config.json")
+const config = require("./config.json");
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
 
 client.once("ready", async () => {
     console.log(`${client.user.username} is online.`);
-    client.user.setPresence({ activities: [{ name: `mention or tag me to talk!`, type: ActivityType.Playing }], status: 'online' })
+    client.user.setPresence({ activities: [{ name: `mention or tag me to talk!`, type: ActivityType.Playing }], status: 'online' });
 });
 
 client.on("messageCreate", async message => {
     if (message.author.bot) return;
 
-    if (message.channel.id !== config.chatID){
-        if(!message.mentions.has(client.user)) return;
-    } 
+    if (!config.chatIDs.includes(message.channel.id)) {
+        if (!message.mentions.has(client.user)) return;
+    }
 
-    const msgText = message.mentions.has(client.user) 
+    const msgText = message.mentions.has(client.user)
         ? message.content.split(" ").slice(1).join(" ")
         : message.content;
 
-    if (!msgText) return
+    if (!msgText) return;
 
     message.channel.sendTyping();
 
@@ -33,19 +33,16 @@ client.on("messageCreate", async message => {
         const chat = await characterAI.createOrContinueChat(config.characterID);
         const response = await chat.sendAndAwaitResponse(`${msgText}`, true);
 
-        return response
+        return response;
     }
 
     try {
-
-        let response = await aiMSG()
-        message.reply(`${response.text}`)
-
+        let response = await aiMSG();
+        message.reply(`${response.text}`);
     } catch (error) {
         console.log(error);
         await message.reply("There was a problem handling the command.");
     }
-
 });
 
-client.login(config.token)
+client.login(config.token);
